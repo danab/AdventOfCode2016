bb6d820865aff93b248230c1805098f3529a249a
Dana Bennett <danabennett10@gmail.com>
2016-12-03 10:31:32 -0500

    Day 3

---
+++ day3.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const fs = require('fs');
+
+const isValidTriangle = ( a, b, c ) => ( a + b > c ) && ( a + c > b ) && ( b + c > a );
+
+fs.readFile( 'day3.txt', ( err, data ) => {
+	if (err) {
+		return console.error(err); // eslint-disable-line no-console
+	}
+
+	let triangles = data
+		.toString()
+		.split('\n')
+		.map( tri => {
+			return tri.split(' ').filter( str => str.trim() !== '' ).map( str => +str.trim());
+		});
+
+	let valid1 = triangles.reduce( (valid, tri) => {
+		return ( isValidTriangle( tri[0], tri[1], tri[2] ) ) ? valid + 1 : valid;
+	}, 0);
+
+	console.log( 'Answer 1 - ', valid1 ); // eslint-disable-line no-console
+
+	let valid2 = 0;
+	let i = 0;
+
+	while ( i < triangles.length ) {
+		if ( isValidTriangle( triangles[i][0], triangles[i+1][0], triangles[i+2][0] ) ) { valid2++; }
+		if ( isValidTriangle( triangles[i][1], triangles[i+1][1], triangles[i+2][1] ) ) { valid2++; }
+		if ( isValidTriangle( triangles[i][2], triangles[i+1][2], triangles[i+2][2] ) ) { valid2++; }
+		i += 3;
+	}
+
+	console.log( 'Answer 2 - ', valid2 ); // eslint-disable-line no-console
+});
